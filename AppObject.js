65aab2621462a2e6e539125c0b0f1a59ab4a3b12
rteliz <9rteliz@stpatrick.org>
2016-10-12 13:57:13 -0500

    finished variables

---
+++ AppObject.js
@@ -0,0 +1,40 @@
+function App(){
+
+    //Declare your instance variables here.
+    this.name
+    this.memory
+    this.state
+
+    //Declare the instance function open here.
+    this.open = function(){
+      
+
+
+
+    };
+    //Declare the instance function sleep here.
+    this.sleep = function(){
+
+
+
+    }
+    //Declare the instance function active here.
+    this.active = function(){
+
+
+
+    }
+    //Declare the instance function close here.
+    this.close = function(){
+
+
+
+    }
+}
+
+//Use the constructor to create 4 App objects below.
+
+
+
+//Declare an array named appList, and place all 4 of your apps in that array.
+//The order does not matter.
