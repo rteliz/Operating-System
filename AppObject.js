0f5ada29141a2e4273232a89f0272f622a817695
rteliz <9rteliz@stpatrick.org>
2016-10-13 13:04:04 -0500

    continued working on app object

--- AppObject.js
+++ AppObject.js
@@ -7,27 +7,21 @@ function App(){
 
     //Declare the instance function open here.
     this.open = function(){
-      
-
-
-
+      this.state = "actve"
     };
     //Declare the instance function sleep here.
     this.sleep = function(){
-
-
-
+      if(){
+        this.state = "sleep"
+        this.memory = this.memory / 2
+      }
     }
     //Declare the instance function active here.
     this.active = function(){
-
-
-
+      
     }
     //Declare the instance function close here.
     this.close = function(){
-
-
 
     }
 }
